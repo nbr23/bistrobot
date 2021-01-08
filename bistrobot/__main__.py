f176b9011279966635a82cc4e2ddbc42d4829eb5
Maxence <max@23.tf>
2021-01-08 02:59:42 +0000

    Make buzzer existence / definition optional

--- bistrobot/__main__.py
+++ bistrobot/__main__.py
@@ -5,7 +5,7 @@ from . import Bistrobot
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument('--gpio_buzzer', required=True, type=int, help='GPIO pin number for the Buzzer')
+    parser.add_argument('--gpio_buzzer', type=int, help='GPIO pin number for the Buzzer')
     parser.add_argument('--gpio_sensor', required=True, type=int, help='GPIO pin number for the Sensor')
     parser.add_argument('--gpio_feeder', required=True, type=int, help='GPIO pin number for the Feeder')
     parser.add_argument('-m', '--meal', action='store_true', help='Serve a meal')
@@ -13,8 +13,7 @@ def main():
     args = parser.parse_args()
 
     if args.meal:
-        gpio_buzzer, gpio_feeder, gpio_sensor = None, None, None
-        gpio_buzzer = int(args.gpio_buzzer)
+        gpio_buzzer = int(args.gpio_buzzer) if args.gpio_buzzer else None
         gpio_feeder = int(args.gpio_feeder)
         gpio_sensor = int(args.gpio_sensor)
         garcon = Bistrobot(gpio_feeder, gpio_sensor)
